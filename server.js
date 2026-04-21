f7a51d5d6b281c82966836829ca7e1d32d3c2526
ReolusMaximus <roeleatssushi@outlook.com>
2026-04-21 22:07:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ console.log("SERVER STARTED");
 
 app.use(express.json());
 app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });

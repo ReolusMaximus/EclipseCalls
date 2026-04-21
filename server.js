d42d91ecbb457c3b3ec33b2e9d98434207cdccfc
ReolusMaximus <roeleatssushi@outlook.com>
2026-04-21 23:18:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,91 +1,73 @@
-console.log("SERVER FILE IS RUNNING");
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-
-console.log("SERVER STARTED");
+const path = require("path");
 
 app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// callcode → socket.id
 let users = {};
-
-app.post("/register", (req, res) => {
-    const { name, callcode } = req.body;
-
-    if (users[callcode]) {
-        return res.send({ success: false, message: "Callcode taken" });
-    }
-
-    users[callcode] = { name, socketId: null };
-    res.send({ success: true, message: "Registered!" });
-});
 
 io.on("connection", (socket) => {
 
- socket.on("login", ({ callcode }) => {
-    if (!users[callcode]) {
-        users[callcode] = {};
-    }
+    console.log("User connected:", socket.id);
 
-    users[callcode].socketId = socket.id;
-    socket.callcode = callcode;
+    // LOGIN
+    socket.on("login", ({ callcode }) => {
+        users[callcode] = socket.id;
+        socket.callcode = callcode;
 
-    console.log("User logged in:", callcode);
-});
+        console.log("Logged in:", callcode);
+    });
 
+    // CALL
     socket.on("call-user", ({ targetCallcode, offer }) => {
-        const target = users[targetCallcode];
+        const targetSocket = users[targetCallcode];
 
-        if (!target || !target.socketId) {
+        if (!targetSocket) {
             socket.emit("user-not-found");
             return;
         }
 
-        io.to(target.socketId).emit("incoming-call", {
+        io.to(targetSocket).emit("incoming-call", {
             from: socket.callcode,
             offer
         });
     });
 
-   socket.on("answer-call", ({ to, answer }) => {
-    const targetSocket = users[to]?.socketId;
+    // ANSWER
+    socket.on("answer-call", ({ to, answer }) => {
+        const targetSocket = users[to];
 
-    if (targetSocket) {
-        io.to(targetSocket).emit("call-answered", answer);
-    }
-});
-
-    socket.on("ice-candidate", async (candidate) => {
-    try {
-        const ice = new RTCIceCandidate(candidate);
-
-        if (peer && peer.remoteDescription) {
-            await peer.addIceCandidate(ice);
-            console.log("ICE applied immediately");
-        } else {
-            console.log("Queueing ICE candidate");
-            pendingCandidates.push(ice);
+        if (targetSocket) {
+            io.to(targetSocket).emit("call-answered", answer);
         }
-    } catch (err) {
-        console.error("ICE error:", err);
-    }
-});
+    });
 
+    // ICE
+    socket.on("ice-candidate", ({ to, candidate }) => {
+        const targetSocket = users[to];
+
+        if (targetSocket) {
+            io.to(targetSocket).emit("ice-candidate", candidate);
+        }
+    });
+
+    // DISCONNECT
     socket.on("disconnect", () => {
-        if (socket.callcode && users[socket.callcode]) {
-            users[socket.callcode].socketId = null;
+        if (socket.callcode) {
+            delete users[socket.callcode];
         }
     });
 });
 
 const PORT = process.env.PORT || 3000;
-
 http.listen(PORT, () => {
     console.log("Server running on port", PORT);
 });
